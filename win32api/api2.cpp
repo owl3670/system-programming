866923994167e3d21008a4fcd9484a86ecd50a4c
owl3670 <gnltjr123@gmail.com>
2023-07-09 08:53:33 +0900

    + windows32 API 개념 과 특징

---
+++ win32api/api2.cpp
@@ -0,0 +1,19 @@
+// api2.cpp
+#include <Windows.h>
+
+int main()
+{
+    int n = 0;
+    double d = 3.4;
+
+    DWORD n1 = 10;
+    UINT n2 = 10;
+
+    POINT pt;
+
+    PINT p1;
+    PSTR p2;
+    PCSTR p3;
+
+    MessageBoxA(0, "Hello", "API", MB_OK);
+}
---
+++ win32api/api3.cpp
@@ -0,0 +1,24 @@
+// api3.cpp
+#include <stdio.h>
+#include <Windows.h>
+
+int main()
+{
+    const char* s = "aaa";
+
+    //printf("s");
+    //MessageBoxA(0, s, s, 0);
+
+    __asm
+    {
+        push s
+        call printf // _printf
+        add esp, 4
+
+        push 0
+        push s
+        push s
+        push 0
+        call MessageBoxA // _MessageBoxA@16 __stdcall 방식
+    }
+}
