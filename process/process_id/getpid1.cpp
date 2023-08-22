ca798583b81b3a4c01ed7d1366cfa16472291748
owl3670 <gnltjr123@gmail.com>
2023-08-22 10:13:36 +0900

    + Get ProcessID

---
+++ process/process_id/getpid1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+
+int _tmain()
+{
+    DWORD pid = GetCurrentProcessId();
+    DWORD tid = GetCurrentThreadId();
+
+    _tprintf(_T("PID : %d, TID : %d\n"), pid, tid);
+}
