5122b7f132faacd08a8a9f3783bfa905dab6743c
owl3670 <gnltjr123@gmail.com>
2024-01-02 20:04:42 +0900

    + IAT Hook

---
+++ dll/apihook/apihook1.cpp
@@ -0,0 +1,6 @@
+#include <Windows.h>
+#include <stdio.h>
+
+int main(){
+    MessageBoxA(0, "API Hooking", "AAA", 0);
+}
---
+++ dll/apihook/apihook2.cpp
@@ -0,0 +1,16 @@
+// 32bit debug 모드
+#include <Windows.h>
+#include <stdio.h>
+
+// MessageBoxA 를 후킹할 함수
+// MessageBoxA 함수의 모양(signature)가 동일 해야 한다.
+UINT __stdcall foo(HWND hwnd, const char* s1, const char* s2, UINT btn){
+    printf("foo : %s, %s\n", s1, s2);
+    return 0;
+}
+
+int main(){
+    // MessageBoxA 의 주소를 담은 IAT 항목을 덮어쓴다.
+
+    MessageBoxA(0, "API Hooking", "AAA", 0);
+}
